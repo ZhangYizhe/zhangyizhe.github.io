7efe6076be7833e18b99448ddb687113d344ad7d
Yizhe ZHANG <elecoxy@gmail.com>
2023-03-31 11:13:01 +0800

    Token calculate

--- src/components/Grammar/GrammarView.vue
+++ src/components/Grammar/GrammarView.vue
@@ -1,72 +1,74 @@
 <template>
-  <div style="background-color: #fafafa; padding-bottom: 35px">
-    <div class="container p-3">
-      <button class="button is-link" style="font-size: 0.8rem; font-weight: bold"><i class="bi bi-spellcheck"
-                                                                                     style="margin-right: 5px; font-size: 1rem"></i>Grammar
-      </button>
+    <div style="background-color: #fafafa; padding-bottom: 35px">
+        <div class="container p-3">
+            <button class="button is-link" style="font-size: 0.8rem; font-weight: bold"><i class="bi bi-spellcheck"
+                                                                                           style="margin-right: 5px; font-size: 1rem"></i>Grammar
+            </button>
+        </div>
     </div>
-  </div>
-  <div class="container canvas mb-5" style="margin-top: -30px; background-color: white">
-    <div class="columns is-multiline m-0">
-      <div class="column">
-        <div class="columns is-multiline">
-          <div class="column is-full">
-            <div class="columns is-mobile">
-              <div class="column is-narrow" style="border-bottom: 3px solid #485fc7">
-                <span class="main-color" style="font-weight: bold;">DETECT LANGUAGE</span>
-              </div>
-            </div>
-          </div>
-          <div class="column is-full p-5" style="border-top: 1px solid #cbcbcb; border-right: 1px solid #cbcbcb">
+    <div class="container canvas mb-5" style="margin-top: -30px; background-color: white">
+        <div class="columns is-multiline m-0">
+            <div class="column">
+                <div class="columns is-multiline">
+                    <div class="column is-full">
+                        <div class="columns is-mobile">
+                            <div class="column is-narrow" style="border-bottom: 3px solid #485fc7">
+                                <span class="main-color" style="font-weight: bold;">DETECT LANGUAGE</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column is-full p-5"
+                         style="border-top: 1px solid #cbcbcb; border-right: 1px solid #cbcbcb">
             <textarea @input="resizeTextarea" v-model="leftInput" placeholder="Input" ref="leftInputRef"
                       :disabled="isLoading"></textarea>
-          </div>
-          <div class="column is-full has-text-right"
-               style="border-top: 1px solid #cbcbcb;border-bottom: 1px solid #cbcbcb; border-right: 1px solid #cbcbcb; margin-bottom: -1px">
-            <button class="button is-danger is-outlined"
-                    style="font-size: 0.8rem; font-weight: bold; margin-right: 10px" @click="cleanBtnTap">
-              <i class="bi bi-trash3" style="margin-right: 5px; font-size: 1rem"></i>
-              Clean
-            </button>
-            <button class="button is-link is-outlined" style="font-size: 0.8rem; font-weight: bold;"
-                    :disabled="isLoading" @click="request">
-              <template v-if="isLoading === false">
-                <i class="bi bi-spellcheck" style="margin-right: 5px; font-size: 1rem"></i>
-              </template>
-              <template v-else>
-                <div class="loader-wrapper is-active" style="margin-right: 5px; font-size: 1rem; color: #485fc7">
-                  <div class="loader is-loading"></div>
+                    </div>
+                    <div class="column is-full has-text-right"
+                         style="border-top: 1px solid #cbcbcb;border-bottom: 1px solid #cbcbcb; border-right: 1px solid #cbcbcb; margin-bottom: -1px">
+                        <button class="button is-danger is-outlined"
+                                style="font-size: 0.8rem; font-weight: bold; margin-right: 10px" @click="cleanBtnTap">
+                            <i class="bi bi-trash3" style="margin-right: 5px; font-size: 1rem"></i>
+                            Clean
+                        </button>
+                        <button class="button is-link is-outlined" style="font-size: 0.8rem; font-weight: bold;"
+                                :disabled="isLoading" @click="request">
+                            <template v-if="isLoading === false">
+                                <i class="bi bi-spellcheck" style="margin-right: 5px; font-size: 1rem"></i>
+                            </template>
+                            <template v-else>
+                                <div class="loader-wrapper is-active"
+                                     style="margin-right: 5px; font-size: 1rem; color: #485fc7">
+                                    <div class="loader is-loading"></div>
+                                </div>
+                            </template>
+                            Check
+                        </button>
+                    </div>
                 </div>
-              </template>
-              Check
-            </button>
-          </div>
-        </div>
-      </div>
-      <div class="column">
-        <div class="columns is-multiline">
-          <div class="column is-full">
-            <div class="columns is-mobile">
-              <div class="column is-narrow" style="border-bottom: 3px solid #485fc7">
-                <span class="main-color" style="font-weight: bold;">RESULT</span>
-              </div>
             </div>
-          </div>
-          <div class="column is-full p-5" style="border-top: 1px solid #cbcbcb;">
-            <textarea v-model="rightInput" placeholder="Output" ref="rightInputRef" disabled></textarea>
-          </div>
-          <div class="column is-full has-text-right"
-               style="border-top: 1px solid #cbcbcb;border-bottom: 1px solid #cbcbcb; border-right: 1px solid #cbcbcb; margin-bottom: -1px">
-            <button class="button is-link is-outlined" style="font-size: 0.8rem; font-weight: bold;"
-                    @click="copyBtnTap">
-              <i class="bi bi-clipboard" style="margin-right: 5px; font-size: 1rem"></i>
-              {{ copyBtnStr }}
-            </button>
-          </div>
+            <div class="column">
+                <div class="columns is-multiline">
+                    <div class="column is-full">
+                        <div class="columns is-mobile">
+                            <div class="column is-narrow" style="border-bottom: 3px solid #485fc7">
+                                <span class="main-color" style="font-weight: bold;">RESULT</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column is-full p-5" style="border-top: 1px solid #cbcbcb;">
+                        <textarea v-model="rightInput" placeholder="Output" ref="rightInputRef" disabled></textarea>
+                    </div>
+                    <div class="column is-full has-text-right"
+                         style="border-top: 1px solid #cbcbcb;border-bottom: 1px solid #cbcbcb; border-right: 1px solid #cbcbcb; margin-bottom: -1px">
+                        <button class="button is-link is-outlined" style="font-size: 0.8rem; font-weight: bold;"
+                                @click="copyBtnTap">
+                            <i class="bi bi-clipboard" style="margin-right: 5px; font-size: 1rem"></i>
+                            {{ copyBtnStr }}
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 
 </template>
 
@@ -76,116 +78,120 @@ import {store} from "@/data/store";
 import {Base64} from "js-base64";
 
 export default {
-  name: "GrammarView",
-  data() {
-    return {
-      store,
+    name: "GrammarView",
+    data() {
+        return {
+            store,
 
-      leftInput: '',
-      rightInput: '',
+            leftInput: '',
+            rightInput: '',
 
-      isLoading: false,
+            isLoading: false,
 
-      copyBtnStr: "Copy",
-    }
-  },
-  watch: {
-    leftInput() {
-      const self = this
-      setTimeout(function () {
-        self.resizeTextarea()
-      }, 100);
+            copyBtnStr: "Copy",
+        }
     },
+    watch: {
+        leftInput() {
+            const self = this
+            setTimeout(function () {
+                self.resizeTextarea()
+            }, 100);
+        },
 
-    rightInput() {
-      const self = this
-      setTimeout(function () {
-        self.resizeTextarea()
-      }, 100);
-    }
-  },
-  computed: {
-    token() {
-      return Base64.decode(store.grammarToken);
-    }
-  },
-  mounted() {
-    this.store.tag = 'grammar';
-  },
-  methods: {
-    resizeTextarea() {
-      this.$refs.leftInputRef.style.height = 0 + 'px';
-      this.$refs.rightInputRef.style.height = 0 + 'px';
-
-      let leftHeight = this.$refs.leftInputRef.scrollHeight;
-      let rightHeight = this.$refs.rightInputRef.scrollHeight;
-
-      let finalHeight;
-      if (leftHeight >= rightHeight) {
-        finalHeight = leftHeight
-      } else {
-        finalHeight = rightHeight
-      }
-
-      this.$refs.leftInputRef.style.height = finalHeight + 'px';
-      this.$refs.rightInputRef.style.height = finalHeight + 'px';
+        rightInput() {
+            const self = this
+            setTimeout(function () {
+                self.resizeTextarea()
+            }, 100);
+        }
     },
+    computed: {
+        token() {
+            return Base64.decode(store.grammarToken);
+        }
+    },
+    mounted() {
+        this.store.tag = 'grammar';
+    },
+    methods: {
+        resizeTextarea() {
+            this.$refs.leftInputRef.style.height = 0 + 'px';
+            this.$refs.rightInputRef.style.height = 0 + 'px';
 
-    request() {
+            let leftHeight = this.$refs.leftInputRef.scrollHeight;
+            let rightHeight = this.$refs.rightInputRef.scrollHeight;
 
-      if (this.leftInput === "") {
-        return
-      }
-
-      this.isLoading = true;
-
-      let prompt = "I desire that you serve as a spelling corrector and enhancer. I will converse with you in any language and you will identify the language, reply in the amended version of my text. I want you to substitute my simplified A0-level words and phrases with more sophisticated, advanced words and phrases. Maintain the same meaning. The content is:\\n\\n" + this.leftInput;
-
-      const headers = {
-        'Authorization': 'Bearer ' + this.token
-      };
-
-      axios.post(this.store.aiProxy + '/v1/completions', {
-        model: "text-davinci-003",
-        temperature: 0.2,
-        max_tokens: 3900,
-        prompt: prompt
-      }, {headers})
-          .then((response) => {
-            this.isLoading = false
-
-            this.rightInput = response.data['choices'][0]['text'].trim();
-          })
-          .catch((error) => {
-            this.isLoading = false
-
-            try {
-              const detail = error.response.data['error']['message']
-              alert(detail);
-            } catch (error) {
-              alert(error);
+            let finalHeight;
+            if (leftHeight >= rightHeight) {
+                finalHeight = leftHeight
+            } else {
+                finalHeight = rightHeight
             }
-          });
-    },
 
-    cleanBtnTap() {
-      this.rightInput = ""
-      this.leftInput = ""
-    },
+            this.$refs.leftInputRef.style.height = finalHeight + 'px';
+            this.$refs.rightInputRef.style.height = finalHeight + 'px';
+        },
 
-    copyBtnTap() {
-      const content = this.rightInput
-      navigator.clipboard.writeText(content);
+        request() {
 
-      this.copyBtnStr = "Success";
+            if (this.leftInput === "") {
+                return
+            }
 
-      const self = this
-      setTimeout(function () {
-        self.copyBtnStr = "Copy";
-      }, 400);
+            this.isLoading = true;
 
+            let prompt = "I desire that you serve as a spelling corrector and enhancer. I will converse with you in any language and you will identify the language, reply in the amended version of my text. I want you to substitute my simplified A0-level words and phrases with more sophisticated, advanced words and phrases. Maintain the same meaning. The content is:\\n\\n" + this.leftInput;
+
+            const headers = {
+                'Authorization': 'Bearer ' + this.token
+            };
+
+            axios.post(this.store.aiProxy + '/v1/completions', {
+                model: "text-davinci-003",
+                temperature: 0.2,
+                max_tokens: 4097 - this.wordCount(prompt) - 100,
+                prompt: prompt
+            }, {headers})
+                .then((response) => {
+                    this.isLoading = false
+
+                    this.rightInput = response.data['choices'][0]['text'].trim();
+                })
+                .catch((error) => {
+                    this.isLoading = false
+
+                    try {
+                        const detail = error.response.data['error']['message']
+                        alert(detail);
+                    } catch (error) {
+                        alert(error);
+                    }
+                });
+        },
+
+        wordCount(str) {
+            return str.split(" ").length;
+        },
+
+        cleanBtnTap() {
+            this.rightInput = ""
+            this.leftInput = ""
+        },
+
+        copyBtnTap() {
+            const content = this.rightInput
+            navigator.clipboard.writeText(content);
+
+            this.copyBtnStr = "Success";
+
+            const self = this
+            setTimeout(function () {
+                self.copyBtnStr = "Copy";
+            }, 400);
+
+        }
     }
-  }
 }
 </script>
 
