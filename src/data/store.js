7902b0dc0e7460a9e80dcc6653f4a96906b0e0c7
Yizhe ZHANG <elecoxy@gmail.com>
2023-06-19 10:19:57 +0800

    Update the model

--- src/data/store.js
+++ src/data/store.js
@@ -5,7 +5,7 @@ export const store = reactive({
     isExpended: false,
     aiProxy: 'https://ai.yizheyun.cn',
 
-    modelVersion: 'gpt-3.5-turbo-0613',
+    modelVersion: 'gpt-3.5-turbo',
 
     chatToken: 'c2stOVVjdzVac3V1Vkx3MkhudXo3Y25UM0JsYmtGSndEYkxSaE5WU1IzaUNOTWQwckNB',
     grammarToken: 'c2stRTdmSEU1QVJ3Um5TQ1RKalRGUm9UM0JsYmtGSlBCbFh2cWR2Zmo4T3hTYW1GNUhQ',
